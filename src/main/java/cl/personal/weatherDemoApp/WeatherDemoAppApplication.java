2f54db402e2d139ed66d9ce53feebda79dba2249
alvasp <alexis.vasquez01@gmailcom>
2017-11-02 21:01:21 -0300

    fixes for tomcat deployment

--- src/main/java/cl/personal/weatherDemoApp/WeatherDemoAppApplication.java
+++ src/main/java/cl/personal/weatherDemoApp/WeatherDemoAppApplication.java
@@ -2,11 +2,18 @@ package cl.personal.weatherDemoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(value = "cl.personal.weatherDemoApp")
-public class WeatherDemoAppApplication {
+public class WeatherDemoAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WeatherDemoAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherDemoAppApplication.class, args);
